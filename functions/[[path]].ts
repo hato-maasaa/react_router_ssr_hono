77d594d576f45da6f1e8bd896cfe36c6c13f0a1b
m.hatori <twjujwptw@gmail.com>
2025-02-08 15:35:15 +0900

    コメントの追加

--- functions/[[path]].ts
+++ functions/[[path]].ts
@@ -1,6 +1,6 @@
 // functions/[[path]].ts
 import handle from "hono-react-router-adapter/cloudflare-pages";
-import * as build from "../build/server";
+import * as build from "../build/server"; // 初回ビルドまでは型エラーになりますが、build(npx wrangler)を実行することで解消されます
 import { getLoadContext } from "../load-context";
 import server from "../server";
 
